eede17ace68167e8af35f14224508fed2b184a6c
Jean Carlos <jeanchpaula@gmail.com>
2021-09-28 22:00:09 -0300

    check and change header when scroll

--- src/main.js
+++ src/main.js
@@ -4,4 +4,23 @@ const hamburgerMenu = document.querySelector('.hamburger')
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('is-active')
   menuModal.classList.toggle('active')
+
+  changeHeaderWhenScroll()
+})
+
+const header = document.querySelector('header')
+const headerHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  let hasNotActive = !menuModal.classList.contains('active')
+
+  if (window.scrollY >= screen.height - 71 && hasNotActive) {
+    header.classList.add('bg-dark')
+  } else {
+    header.classList.remove('bg-dark')
+  }
+}
+
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll()
 })
