8156ba214856b772e349c3201ca4d9b024d78068
Jean Carlos <jeanchpaula@gmail.com>
2021-09-29 19:31:43 -0300

    add menu modal operating functions

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 ﻿const menuModal = document.querySelector('.menu-modal')
+const itemsMenu = document.querySelector('.items-menu')
 const hamburgerMenu = document.querySelector('.hamburger')
 
 hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('is-active')
   menuModal.classList.toggle('active')
+  itemsMenu.classList.toggle('show')
+  hamburgerMenu.classList.toggle('is-active')
 
   changeHeaderWhenScroll()
+})
+
+const linksItemsMenu = itemsMenu.querySelectorAll('a')
+linksItemsMenu.forEach(link => {
+  link.addEventListener('click', () => {
+    menuModal.classList.remove('active')
+    itemsMenu.classList.remove('show')
+    hamburgerMenu.classList.remove('is-active')
+
+    changeHeaderWhenScroll()
+  })
 })
 
 const header = document.querySelector('header')
@@ -14,7 +27,7 @@ const headerHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
   let hasNotActive = !menuModal.classList.contains('active')
 
-  if (window.scrollY >= screen.height - 71 && hasNotActive) {
+  if (window.scrollY >= screen.height - 100 && hasNotActive) {
     header.classList.add('bg-dark')
   } else {
     header.classList.remove('bg-dark')
@@ -23,4 +36,13 @@ function changeHeaderWhenScroll() {
 
 window.addEventListener('scroll', () => {
   changeHeaderWhenScroll()
+})
+
+const btnShowProducts = document.querySelector('.btn-show-products')
+const viewProducts = document.querySelector('.view-products')
+const polygonSpan = document.querySelector('.polygon')
+
+btnShowProducts.addEventListener('click', () => {
+  viewProducts.classList.toggle('show')
+  polygonSpan.classList.toggle('open')
 })
