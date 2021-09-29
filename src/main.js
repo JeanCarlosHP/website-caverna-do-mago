564937171edfc08f0be35c3f54dffd443fda3089
Jean Carlos <jeanchpaula@gmail.com>
2021-09-28 21:38:14 -0300

    toggle menu modal class list

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-﻿const hamburgerMenu = document.querySelector('.hamburger')
+﻿const menuModal = document.querySelector('.menu-modal')
+const hamburgerMenu = document.querySelector('.hamburger')
+
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('is-active')
+  menuModal.classList.toggle('active')
 })
